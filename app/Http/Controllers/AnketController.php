3ab77e00bfc4240ea7d93fcf53f4874931b8caf0
unknown <dev4.git@jit.vn>
2021-03-26 10:55:07 +0700

    template

---
+++ app/Http/Controllers/AnketController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Anket;
+use Illuminate\Http\Request;
+
+class AnketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Anket  $anket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anket $anket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Anket  $anket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anket $anket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Anket  $anket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anket $anket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Anket  $anket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anket $anket)
+    {
+        //
+    }
+}
---
+++ database/factories/AnketFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Anket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnketFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Anket::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
